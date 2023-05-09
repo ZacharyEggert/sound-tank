c6f8938bce7b588aec3ce399a93ec31f92ae399c
Zachary Eggert <Eggert.Zachary@gmail.com>
2023-05-09 12:06:36 -0700

    refactor

---
+++ src/methods/index.test.ts
@@ -0,0 +1,33 @@
+import { expect, it, suite } from 'vitest';
+
+import Reverb from '../Reverb';
+import { config } from 'dotenv';
+import { getMyRoot } from '.';
+
+config();
+
+suite(
+  'getMyRoot',
+  () => {
+    const { REVERB_API_KEY } = process.env;
+    const reverb = new Reverb({ apiKey: REVERB_API_KEY });
+
+    // it('should fetch a valid response', async ({ expect }) => {
+    //   const response = await getMyRoot(reverb);
+    //   //expect a 200 response code
+    //   expect(response.status).toBeLessThan(300);
+    // });
+
+    it.concurrent(
+      'should fetch a response with a _links object',
+      async ({ expect }) => {
+        const response = await getMyRoot<{ _links: Record<string, any> }>(
+          reverb,
+        );
+        //expect the response to have a Links object
+        expect(response.data._links).toBeDefined();
+      },
+    );
+  },
+  { timeout: 60000 },
+);
