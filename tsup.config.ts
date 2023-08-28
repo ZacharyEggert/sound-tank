09474348838e5cf563f91b2f3be09ebe78025e92
Zachary Eggert <Eggert.Zachary@gmail.com>
2023-08-28 15:55:35 -0700

    tsup config -esm module default - fix bad log test

---
+++ tsup.config.ts
@@ -0,0 +1,17 @@
+import { defineConfig } from 'tsup';
+
+export default defineConfig({
+  entry: ['src/index.ts'],
+  splitting: true,
+  format: ['cjs', 'esm'],
+  dts: true,
+  sourcemap: true,
+  tsconfig: 'tsconfig.json',
+  clean: true,
+  target: 'es2020',
+  platform: 'node',
+  external: ['axios'],
+  bundle: true,
+  outDir: 'dist',
+  keepNames: true,
+});
